e979e5b9800c129312dc53f2a0fbc979e391a029
asd284897227 <550443096@qq.com>
2021-04-12 13:40:23 +0800

    尝试解决无法注入InfluxRepositry的问题；

--- src/main/java/com/rcd/fiber/repository/InfluxRepository.java
+++ src/main/java/com/rcd/fiber/repository/InfluxRepository.java
@@ -1,11 +1,10 @@
 package com.rcd.fiber.repository;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.rcd.fiber.annotation.IJsonQuery;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
 
-@Repository
+@Mapper
 public interface InfluxRepository {
     @IJsonQuery("select * from telemetry where device_name = #{device_name} and data_name = #{data_name} and site_name = #{site_name} order by time desc limit 1")
     public JSONObject getTelemetryInfo(JSONObject params);
--- src/main/java/com/rcd/fiber/service/TelemetryService.java
+++ src/main/java/com/rcd/fiber/service/TelemetryService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.annotation.Resource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -26,7 +27,7 @@ import java.util.List;
 @Transactional
 public class TelemetryService {
     private final Logger log = LoggerFactory.getLogger(TelemetryService.class);
-    @Autowired
+    @Resource(type = InfluxRepository.class)
     private InfluxRepository influxRepository;
 
     //王伟（2020-1-11）： 根据站点名称，查询voltDB内的遥测数据
